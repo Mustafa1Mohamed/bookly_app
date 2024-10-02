91f911bad166c689fec7e5cd2e556bb63da524af
Mustafa1Mohamed <mm0118250@gmail.com>
2024-10-02 23:48:37 +0300

    create custom book details app bar

--- lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/presentaion/views/widgets/best_seller_list_view_item.dart
@@ -1,77 +1,84 @@
 import 'package:bookly_app/Features/home/presentaion/views/widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 24),
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.6 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      AssetsData.logo,
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: SizedBox(
+        height: 125,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 24),
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.6 / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.red,
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.logo,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .5,
-                    child: Text(
-                      'Harry Potter and the Goblet of Fire',
-                      style: Styles.textStyle20
-                          .copyWith(fontFamily: kGTSectraFine),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  const Text(
-                    'J.K. Rowlling',
-                    style: Styles.textStyle14,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '19.99 \$',
-                        style: Styles.textStyle20
-                            .copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      const Spacer(),
-                      const BookRating(),
-                    ],
-                  )
-                ],
+              const SizedBox(
+                width: 30,
               ),
-            ),
-          ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .5,
+                      child: Text(
+                        'Harry Potter and the Goblet of Fire',
+                        style: Styles.textStyle20
+                            .copyWith(fontFamily: kGTSectraFine),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    const Text(
+                      'J.K. Rowlling',
+                      style: Styles.textStyle14,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '19.99 \$',
+                          style: Styles.textStyle20
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        const Spacer(),
+                        const BookRating(),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,8 +1,11 @@
+import 'package:bookly_app/Features/home/presentaion/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentaion/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
-abstract class AppRouter{
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsBiew';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -12,8 +15,11 @@ abstract class AppRouter{
       GoRoute(
         path: '/homeView',
         builder: (context, state) => const HomeView(),
-      )
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      ),
     ],
   );
-
 }

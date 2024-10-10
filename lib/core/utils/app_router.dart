5361dedbb9daae4810cee06146daad59a781a112
Mustafa1Mohamed <mm0118250@gmail.com>
2024-10-10 21:19:05 +0300

    fetch similar books done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,13 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/presentaion/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/Features/home/presentaion/views/book_details_view.dart';
 import 'package:bookly_app/Features/home/presentaion/views/home_view.dart';
 import 'package:bookly_app/Features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -20,7 +26,12 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+          child:  BookDetailsView(bookModel: state.extra as BookModel,),
+        ),
       ),
       GoRoute(
         path: kSearchView,

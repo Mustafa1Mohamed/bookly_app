f8e43f17574c1ccc8d0ee6134f09587066e04b67
Mustafa1Mohamed <mm0118250@gmail.com>
2024-10-10 20:45:53 +0300

    fix issues with book model

--- lib/Features/home/presentaion/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentaion/views/widgets/featured_list_view.dart
@@ -24,7 +24,7 @@ class FeaturedBooksListView extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 8),
                   child: CustomBookImage(
                     imageURL:
-                        state.books[index].volumeInfo.imageLinks.thumbnail,
+                        state.books[index].volumeInfo.imageLinks?.thumbnail ?? '',
                   ),
                 );
               },
